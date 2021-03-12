16db367557fa2b146b0cff169235f4ee187e162b
Mikaela Mazey <maz16001@byui.edu>
2021-03-11 18:03:56 -0700

    console and roster

---
+++ console.py
@@ -0,0 +1,26 @@
+
+class Console:
+
+    def getInput(self, message):
+        return input(message)
+
+    def display(self, message):
+        print(message)
+
+    def getPlayerName(self, player):
+        return(input(f"Enter a name for player {player}"))
+    
+    def doTurn(self, name):
+        x = 0
+        invalid = true
+        while invalid
+            print(f"{name}'s turn: ")
+            x = (int(input("What is your guess? ")))
+                if x >=1000 and <= 9999:
+                    invalid = false
+                else:
+                    print("Incorrect. Choose a number between 1000 and 9999")
+        return x
+    
+    
+        
---
+++ roster.py
@@ -0,0 +1,16 @@
+import player
+
+class Roster:
+
+    def __init__(self):
+        self._players = []
+    
+    def addPlayer(self, player):
+    #adding the player to the list above
+        self.players.append(player)
+
+    def getPlayers(): 
+        return self.players
+
+    def getNumPlayers(self, player):
+        return len(self.players)
