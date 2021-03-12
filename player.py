b8fe98f52a5e7684fcf595491df541190c45214b
Samson Idoko <Samsonidoko3@byui.edu>
2021-03-12 15:42:46 -0700

    Class Player

---
+++ player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self, name):
+        self._name = name
+        
+    def getName(self):
+        return self._name
+    
+    def setName(self, name):
+        self._name = name
